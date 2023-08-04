e4668a6c7f9e99633347a49e10a1f844cf7460d2
Krystiankr <162847@student.uwm.edu.pl>
2023-08-04 09:21:26 +0200

    Configure YAML file

--- src/model/train.py
+++ src/model/train.py
@@ -7,7 +7,11 @@ import os
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
+import mlflow
+
+mlflow.autolog()
 
 # define functions
 def main(args):
@@ -33,8 +37,10 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-# TO DO: add function to split data
-
+def split_data(df):
+    # split data
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    return train_test_split(X, y, test_size=0.30, random_state=0)
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
